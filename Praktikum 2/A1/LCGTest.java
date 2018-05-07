b8120734d36a6e06c9f4acb1508b858af8f247c9
Kevin Hüsgen <kevin@huesgen.at>
2018-05-07 19:03:20 +0200

    Cleanup

--- Praktikum 2/A1/LCGTest.java
+++
@@ -1,9 +0,0 @@
-
-public class LCGTest {
-	public static void main(String[] args) {
-		LCG test = new LCG(5);
-		for (int i = 0; i < 256; i++) {
-			System.out.println(test.nextInt() & 0x000000FF);
-		}
-	}
-}
